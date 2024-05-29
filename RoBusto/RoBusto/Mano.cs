72f6462f3881676dbedc048dc7c984c2e5abc797
soulparade <153643201+soulparade@users.noreply.github.com>
2024-05-29 20:44:24 +0200

    Add files via upload

---
+++ RoBusto/RoBusto/Mano.cs
@@ -0,0 +1,5 @@
+namespace RoBusto;
+public class Mano(List<Dito> dita)
+{
+    public List<Dito> Dita { get; set; } = dita;
+}
---
+++ RoBusto/RoBusto/Spalla.cs
@@ -0,0 +1,6 @@
+namespace RoBusto;
+
+public class Spalla(List<Servo> servo)
+{
+    public List<Servo> Servi { get; set; } = servo;
+}
